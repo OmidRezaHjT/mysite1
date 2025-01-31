35744c2bb4f546fa2154a400fe235322014c0baa
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-01-31 12:07:15 +0330

    create a render html files
    added context folder and html files

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home_page(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request,'index.html')
 def about_page(request):
-    return HttpResponse('<h1>hey my name is omidreza</h1>')
+    return render(request,'about.html')
 def contact_page(request):
-    return HttpResponse('<h1>hey if u want call me this is my number 120030131</h1>')
+    return render(request,'contact.html')
