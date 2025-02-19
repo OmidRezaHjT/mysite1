50ecfa9f75df32fec7cffa19ad54358741f76426
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-02-19 18:36:08 +0330

    set view counter on posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.utils import timezone
+from blog.models import post
+
+def increment_post_views(post_view):
+    post_view.counted_views += 1
+    post_view.save()
+
 def blog_page(request):
-    return render(request,'blog/blog-home.html')
+    posts = post.objects.filter(publish_date__lte=timezone.now())
+    for p in posts:
+        increment_post_views(p)
+    context = {'posts': posts}
+    return render(request,'blog/blog-home.html', context)
 def single_page(request):
     return render(request,'blog/blog-single.html')
