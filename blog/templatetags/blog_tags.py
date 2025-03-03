92509859bc8b236ec9a307863a41a31fd6e0b64e
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-04 03:17:52 +0330

    add category tag and latestpost tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,15 @@
 from django import template
-from blog.models import post
+from blog.models import post , category
 register = template.Library()
-@register.template_tag(name = 'totalposts')
-def function():
-    posts = post.objects.filter(status = 1).count()
-    return posts
+@register.inclusion_tag('blog/blog-latestposts.html')
+def latestposts(arg=4):
+    posts = post.objects.filter(status = 1).order_by('publish_date')[:arg]
+    return {'posts': posts}
+@register.inclusion_tag('blog/blog-postcat.html')
+def postcategories():
+    posts = post.objects.filter(status = 1)
+    categories = category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+    return {'categories' :cat_dict}
