8a67323732e4e3321583a14b077c2d89617f3618
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-08 04:50:33 +0330

    add blog pagination
    update views and blog-home

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
 from blog.models import post
+from django.core.paginator import Paginator , PageNotAnInteger , EmptyPage
 
 
 
@@ -10,6 +11,15 @@ def blog_page(request,cat_name=None,author_username=None):
         posts = posts.filter(category__name=cat_name)
     if author_username:
         posts = posts.filter(author__username=author_username)
+    posts= Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'posts': posts}
     return render(request,'blog/blog-home.html', context)
 def single_page(request,pid):
