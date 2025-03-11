50c37e70762d2dfdcbb6423bf7ba041eb4fcae24
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-11 14:45:37 +0330

    update latest posts section to the main page

--- blog/templatetags/website_tags.py
+++ blog/templatetags/website_tags.py
@@ -1,7 +1,10 @@
 from django import template
 from blog.models import post
+from django.utils import timezone
+
 register = template.Library()
 @register.inclusion_tag('website/latest_posts.html')
 def latestposts():
-    posts = post.objects.filter(status = 1).order_by('-publish_date')[:6]
+    posts =post.objects.filter(publish_date__lte=timezone.now(),status=1)
+    posts = posts.filter(status = 1).order_by('-publish_date')[:6]
     return {'posts': posts}
