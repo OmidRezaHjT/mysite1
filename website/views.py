0b625360c00d9acba59672fac3e18d4baa1b0df6
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-14 04:07:19 +0330

    Add success and error message handling in forms
    Updated views.py and contact.html to add success/error messages

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from blog.models import post
 from website.forms import *
+from django.contrib import messages
 
 def home_page(request):
     posts = post.objects.all()
@@ -14,6 +15,7 @@ def contact_page(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, "Ur ticket successfully saved")
     form = ContactForm() 
     return render(request,'website/contact.html',{'form':form})
 
