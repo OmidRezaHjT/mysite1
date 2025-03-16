9b36441dd1a3a789e3de9f0415d27878e527b21e
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-17 03:17:20 +0330

    Use humanize naturaltime for publish_date and add humanize, extensions, and sites to installed apps

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'django.contrib.humanize',
+    'django_extensions',
+    'django.contrib.sites',
     'blog'
 ]
 
