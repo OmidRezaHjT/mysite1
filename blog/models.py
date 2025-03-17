152acb662842f59282b271b78a740451de281bc8
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-03-17 19:22:27 +0330

    Add sitemaps for static pages and blog posts
    - Add StaticViewSitemap for static pages
    - Add BlogSitemap to list published blog posts

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class category(models.Model):
@@ -25,3 +26,5 @@ class post(models.Model):
     
     def __str__(self):
         return "{} - {}".format(self.title,self.id)
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid': self.id})
