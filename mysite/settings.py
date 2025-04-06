eb26b83b7ec39483d73e117ec7da755eef904578
OmidRezaHjT <omidreza.hjt@gmail.com>
2025-04-07 02:50:17 +0330

    Add initial prerequisites for deploying the website on hosting
    - Created a 'settings' directory to organize environment-specific configurations (development and production)
    - Split the original settings.py into two separate files: dev.py for development and prod.py for production
    - Updated the project to load settings according to the environment

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,17 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1-s)9wtb654)#mvl%985emq2$e4fqb24q4=+s%1ny^s&b3hqv8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -51,8 +40,6 @@ INSTALLED_APPS = [
     'blog',
     'accounts',
 ]
-#sites framework
-SITE_ID = 2
 #summernote configs
 UMMERNOTE_THEME = 'bsf'
 SUMMERNOTE_CONFIG = {
@@ -123,15 +110,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 
@@ -172,12 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-STATICFILES_DIRS = [
-    BASE_DIR / "statics"
-]
+
+
 
 
 # Default primary key field type
@@ -195,11 +170,3 @@ from django.urls import reverse_lazy
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# Forget pass
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'testforsite04@gmail.com'  
-EMAIL_HOST_PASSWORD = 'hwly hikp rkan cjbs' 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
